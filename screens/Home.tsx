144277ca095fc9302bafebb7260d1c4d2c62f89a
JordanMalekeraVives <jordan.emmanuelmalekera@gmail.com>
2022-10-25 23:51:22 +0200

    Added Home screen

---
+++ screens/Home.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from '../components/Themed';
+import { StyleSheet } from 'react-native'
+
+export default function Home() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    img: {
+        width: 20,
+        height: 20
+    }
+})
