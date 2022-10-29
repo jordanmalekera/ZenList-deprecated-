a63548efda114b840d8047d8a1967560bbbf4717
JordanMalekeraVives <91864963+jordanmalekera@users.noreply.github.com>
2022-10-29 22:40:23 +0200

    added screen boilerplate

--- screens/Discover.tsx
+++ screens/Discover.tsx
@@ -1,13 +1,41 @@
 import { Text, View } from '../components/Themed';
 import { StyleSheet } from 'react-native'
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../app/store';
+
 
 export default function Discover() {
+    const category = useSelector((state: RootState) => state.categories.value)
+    if(category.movies_series) {
         return (
             <View>
-                <Text>eeeee</Text>
+                <Text>Movies</Text>
             </View>
         )
+    }
+    else if(category.anime_manga) {
+        return (
+            <View>
+                <Text>Anime</Text>
+            </View>
+        )
+    }
+    else if(category.games) {
+        return (
+            <View>
+                <Text>Games</Text>
+            </View>
+        )
+    }
+    else {
+        return(
+            <View style={{backgroundColor:"red"}}>
+                <Text>Icon</Text>
+            </View>
+        )
+    }
+    
 }
 
 const styles = StyleSheet.create({
--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,12 +1,36 @@
 import { Text, View } from '../components/Themed';
 import { StyleSheet } from 'react-native'
+import { useSelector } from 'react-redux';
+import { RootState } from '../app/store';
 
 export default function Search() {
-    return (
-        <View>
-            <Text>This is home</Text>
-        </View>
-    )
+    const category = useSelector((state: RootState) => state.categories.value)
+    if(category.movies_series) {
+        return (
+            <View>
+                <Text>Movies</Text>
+            </View>
+        )
+    }
+    else if(category.anime_manga) {
+        return (
+            <View>
+                <Text>Anime</Text>
+            </View>
+        )
+    }
+    else if(category.games) {
+        return (
+            <View>
+                <Text>Games</Text>
+            </View>
+        )
+    }
+    else {
+        return(
+            <View/>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
