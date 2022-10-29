18ff2b3e0d7da533edfc9b5271a2b9fd376ef59c
JordanMalekeraVives <91864963+jordanmalekera@users.noreply.github.com>
2022-10-29 12:26:58 +0200

    Changed the navigation index

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -4,131 +4,143 @@
  *
  */
 
-//Import icons
-import { FontAwesome } from '@expo/vector-icons';
-
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+//import navigation
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import * as React from 'react';
-import { ColorSchemeName, Pressable } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import LinkingConfiguration from './LinkingConfiguration';
+
+//Import screens and components
+import Discover from '../screens/Discover';
+import Search from '../screens/Search';
+import Lists from '../screens/Lists';
+import Profile from '../screens/Profile';
+import DataChooserModal from '../components/DataChooserModal';
+
+//import app theme colors
+import { ColorSchemeName } from 'react-native';
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
 
+//import icon
+import { Octicons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 
-//Import screens
-import Discover from '../screens/Discover';
+//import types
+import { RootStackParamList, RootTabParamList } from '../types';
 
-//Import types for typescript
-import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
-
-import LinkingConfiguration from './LinkingConfiguration';
+/**  
+ * app navigation container 
+ */
 
 export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
-  return (
-    //Manages our navigation tree
-    <NavigationContainer
-      linking={LinkingConfiguration}
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <RootNavigator />
-    </NavigationContainer>
-  );
+    return (
+        //Manages our navigation tree
+        <NavigationContainer
+            linking={LinkingConfiguration}
+            theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+            <RootNavigator />
+        </NavigationContainer>
+    );
 }
 
 /**
  * A root stack navigator is often used for displaying modals on top of all other content.
  * https://reactnavigation.org/docs/modal
  */
+
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} />
-      {/* <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
-      <Stack.Group screenOptions={{ presentation: 'modal' }}>
-        <Stack.Screen name="Modal" component={ModalScreen} />
-      </Stack.Group> */}
-    </Stack.Navigator>
-  );
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }}/>
+        </Stack.Navigator>
+    );
 }
 
 /**
  * A bottom tab navigator displays tab buttons on the bottom of the display to switch screens.
  * https://reactnavigation.org/docs/bottom-tab-navigator
  */
-const BottomTab = createBottomTabNavigator<RootTabParamList>();
+ const BottomTab = createBottomTabNavigator<RootTabParamList>();
 
-function BottomTabNavigator() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <BottomTab.Navigator
-      initialRouteName="Discover"
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme].tint,
-      }}>
-      <BottomTab.Screen 
-        name="Discover"
-        component={Discover}
-      />
-        <BottomTab.Screen
-            name="Search"
-            component={Discover}
-        />
-        <BottomTab.Screen
-            name="Chooser"
-            component={Discover}
-        />
-        <BottomTab.Screen
-            name="Lists"
-            component={Discover}
-        />
-        <BottomTab.Screen
-            name="Profile"
-            component={Discover}
-        />
-        
-      {/* <BottomTab.Screen
-        name="TabOne"
-        component={TabOneScreen}
-        options={({ navigation }: RootTabScreenProps<'TabOne'>) => ({
-          title: 'Tab One',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Pressable
-              onPress={() => navigation.navigate('Modal')}
-              style={({ pressed }) => ({
-                opacity: pressed ? 0.5 : 1,
-              })}>
-              <FontAwesome
-                name="info-circle"
-                size={25}
-                color={Colors[colorScheme].text}
-                style={{ marginRight: 15 }}
-              />
-            </Pressable>
-          ),
-        })}
-      />
-      <BottomTab.Screen
-        name="TabTwo"
-        component={TabTwoScreen}
-        options={{
-          title: 'Tab Two',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      /> */}
-    </BottomTab.Navigator>
-  );
-}
-
-/**
- * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
- */
-// function TabBarIcon(props: {
-//   name: React.ComponentProps<typeof FontAwesome>['name'];
-//   color: string;
-// }) {
-//   return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
-// }
+ export function BottomTabNavigator(props:any) {
+     const colorScheme = useColorScheme();
+     const EmptyComponent = () => null;
+ 
+     return (
+         <BottomTab.Navigator
+             initialRouteName="Discover"
+             backBehavior="history"
+             screenOptions={{
+                 tabBarStyle: { backgroundColor: Colors[colorScheme].background, borderTopWidth: 0 },
+                 tabBarActiveTintColor: Colors[colorScheme].tint,
+             }}>
+             <BottomTab.Screen
+                 name="Discover"
+                 options={({route, navigation}) => ({
+                     headerStyle: { backgroundColor: Colors[colorScheme].background },
+                     tabBarIcon: ({ color }) => <TabBarIconMat name="explore" color={color} />,
+                 })}>
+                 {() => <Discover chosen={props.chosen}/>}
+             </BottomTab.Screen>
+             <BottomTab.Screen
+                 name="Search"
+                 options={{
+                     headerStyle: { backgroundColor: Colors[colorScheme].background },
+                     tabBarIcon: ({ color }) => <TabBarIcon name="search" color={color} />
+                 }}>
+                     {() => <Search chosen={props.chosen}/>}
+                 </BottomTab.Screen>
+             <BottomTab.Screen
+                 name="Chooser"
+                 component={EmptyComponent}
+                 options={({ navigation }) => ({
+                     headerShown: false,
+                     tabBarItemStyle: { backgroundColor: "green", borderRadius: 200, flex: 0.6},
+                     headerStyle: { backgroundColor: Colors[colorScheme].background },
+                     tabBarLabel: "",
+                     tabBarLabelStyle: { display: 'none' },
+                     tabBarIcon: () => <TabBarIcon name="plus" color={Colors[colorScheme].tint} />,
+                     tabBarButton: () => <DataChooserModal style={{bottom: 0, marginHorizontal: 10}} />
+                 })}
+             />
+             <BottomTab.Screen
+                 name="Lists"
+                 options={{
+                     headerStyle: { backgroundColor: Colors[colorScheme].background },
+                     tabBarIcon: ({ color }) => <TabBarIcon name="list-unordered" color={color} />
+                 }}>
+                     {() => <Lists chosen={props.chosen}/>}
+             </BottomTab.Screen>
+             <BottomTab.Screen
+                 name="Profile"
+                 options={{
+                     headerStyle: { backgroundColor: Colors[colorScheme].background },
+                     tabBarIcon: ({ color }) => <TabBarIcon name="person" color={color} />
+                 }}>
+                     {() => <Profile chosen={props.chosen}/>}
+                 </BottomTab.Screen>
+         </BottomTab.Navigator>
+     );
+ }
+ 
+ /**
+  * built-in icon families and icons
+  * https://icons.expo.fyi/
+  */
+ function TabBarIcon(props: {
+     name: React.ComponentProps<typeof Octicons>['name'];
+     color: string;
+ }) {
+     return <Octicons size={30} style={{ marginBottom: -3 }} {...props} />;
+ }
+ 
+ function TabBarIconMat(props: {
+     name: React.ComponentProps<typeof MaterialIcons>['name'];
+     color: string;
+ }) {
+     return <MaterialIcons size={30} style={{ marginBottom: -3 }} {...props} />;
+ }
+ 
