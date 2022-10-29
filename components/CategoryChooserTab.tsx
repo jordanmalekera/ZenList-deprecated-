ca587613396046cb5c1a11934d68020a156adf7e
JordanMalekeraVives <91864963+jordanmalekera@users.noreply.github.com>
2022-10-30 01:52:57 +0200

    Improved category chooser

--- components/CategoryChooserTab.tsx
+++ components/CategoryChooserTab.tsx
@@ -4,54 +4,51 @@ import { MaterialIcons, Ionicons, MaterialCommunityIcons } from '@expo/vector-ic
 import { changeCategory } from '../features/categorySlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../app/store';
+import { useRef, useState } from 'react';
+import React from 'react';
 
 export default function CategoryChooserTab() {
-
-  return (
-    <View style={[styles.container]}>
-      <TouchableWithoutFeedback onPress={() => {
-      }}>
-        <Animated.View style={[styles.button, styles.menu]}>
-          <MaterialIcons name="movie" size={30} color="white" />
-        </Animated.View>
-      </TouchableWithoutFeedback>
-
-      <TouchableWithoutFeedback onPress={() => {
-      }}>
-        <Animated.View style={[styles.button, styles.third]}>
-          <MaterialCommunityIcons name="syllabary-hiragana" size={30} color="white" />
-        </Animated.View>
-      </TouchableWithoutFeedback>
-
-      <CategoryChooserItem />
-
-    </View>
-  );
-}
-
-interface CategoryChooserItemProp {
-  
-}
-
-export function CategoryChooserItem(prop:any) {
   const dispatch = useDispatch()
   const category = useSelector((state: RootState) => state.categories.value)
-  return(
-    <TouchableWithoutFeedback onPress={() => {
+  const pressedElement = useRef()
+  const btnArray = [
+    <TouchableWithoutFeedback key={0} onPress={() => {
       dispatch(changeCategory("games"));
+      changeBtnOrder([btnArray[0], btnArray[1], btnArray[2]])
+
     }}>
-      <Animated.View style={[styles.button, styles.secondary]}>
+      <Animated.View style={[styles.button, styles.first]}>
+        <MaterialIcons name="movie" size={30} color="white" />
+      </Animated.View>
+    </TouchableWithoutFeedback>,
+
+    <TouchableWithoutFeedback key={1} onPress={(e) => {
+      dispatch(changeCategory("movies_series"));
+      changeBtnOrder([btnArray[1], btnArray[2], btnArray[0]])
+    }}>
+      <Animated.View style={[styles.button, styles.second]}>
+        <MaterialCommunityIcons name="syllabary-hiragana" size={30} color="white" />
+      </Animated.View>
+    </TouchableWithoutFeedback>,
+
+    <TouchableWithoutFeedback key={2} onPress={(e) => {
+      dispatch(changeCategory("anime_manga"));
+      changeBtnOrder([btnArray[2], btnArray[0], btnArray[1]])
+    }}>
+      <Animated.View style={[styles.button, styles.third]}>
         <Ionicons name="game-controller" size={30} color="white" />
       </Animated.View>
     </TouchableWithoutFeedback>
-  )
+  ]
+  const [btnOrdered, changeBtnOrder] = useState([btnArray[0], btnArray[1], btnArray[2]])
+
+  return React.createElement(View, { style: styles.container }, btnOrdered)
 }
 
 const styles = StyleSheet.create({
   container: {
     position: 'relative',
     justifyContent: "center",
-    bottom: 0,
     marginHorizontal: 10
   },
 
@@ -63,18 +60,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  menu: {
-    backgroundColor: "#F02A4B"
+  first: {
+    backgroundColor: "red",
   },
 
-  secondary: {
-    position: "absolute",
+  second: {
     backgroundColor: "green",
-    bottom: 70
+    position: 'absolute',
   },
   third: {
-    position: "absolute",
     backgroundColor: "blue",
-    bottom: 140
+    position: 'absolute',
   }
 });
