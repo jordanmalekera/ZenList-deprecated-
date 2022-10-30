f7b66d67874479c7515af9fde5551593bc7d8548
JordanMalekeraVives <91864963+jordanmalekera@users.noreply.github.com>
2022-10-31 00:42:05 +0100

    Changed intro

--- screens/login/Intro.tsx
+++ screens/login/Intro.tsx
@@ -1,12 +1,69 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
 import React from 'react'
+import { StyleSheet, Image, TouchableOpacity, ColorSchemeName } from 'react-native'
+import { View, Text, Header } from '../../components/Themed'
+import Colors from '../../constants/Colors'
+import useColorScheme from '../../hooks/useColorScheme';
+
+
 
 export default function Intro() {
-  return (
-    <View>
-      <Text>Intro</Text>
-    </View>
-  )
+    const navigation = useNavigation()
+    const colorScheme = useColorScheme();
+    let reverseColorScheme: NonNullable<ColorSchemeName>
+    (colorScheme == 'light') ? reverseColorScheme = 'dark' : reverseColorScheme = 'light';
+    
+    return (
+        <View style={styles.view}>
+            <Image source={require('../../assets/images/icon_black.png')} style={styles.img} />
+            <View >
+                <Header style={styles.header}>Welcome</Header>
+                <Text style={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae eum, blanditiis nam aspe delectus rem sunt iste ad minus</Text>
+            </View>
+            <TouchableOpacity style={[styles.button, {backgroundColor: Colors[reverseColorScheme].background}]} onPress={() => {
+                    navigation.navigate('Login')
+                }}>
+                <Text style={{color: Colors[reverseColorScheme].text, fontSize: 20}}>Continue</Text>
+            </TouchableOpacity>
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    view: {
+        height: '100%',
+        justifyContent: 'space-between',
+        paddingVertical: '20%'
+    },
+
+    img: {
+        height: '50%',
+        width: '90%',
+        alignSelf: 'center'
+    },
+
+    header: {
+        fontSize: 34,
+        fontFamily: 'open-sans',
+        fontWeight: '700',
+        marginHorizontal: '8%',
+        marginBottom: 20,
+        textAlign: 'center'
+    },
+    
+    text: {
+        fontSize: 14,
+        lineHeight: 28,
+        marginHorizontal: '8%',
+        textAlign: 'center'
+    },
+    button: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginHorizontal: '10%',
+        padding: 14,
+        borderRadius: 12
+    }
+    
+
+})
