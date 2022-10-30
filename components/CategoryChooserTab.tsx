500103b2b2d1e38411d4e8777be1a5a85384c77d
JordanMalekeraVives <91864963+jordanmalekera@users.noreply.github.com>
2022-10-30 11:41:47 +0100

    Updated animated to normal views

--- components/CategoryChooserTab.tsx
+++ components/CategoryChooserTab.tsx
@@ -1,48 +1,46 @@
-import { Animated, TouchableWithoutFeedback, StyleSheet, ViewProps } from 'react-native';
+import { TouchableWithoutFeedback, StyleSheet } from 'react-native';
 import { View } from './Themed';
 import { MaterialIcons, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons'
 import { changeCategory } from '../features/categorySlice';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../app/store';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import React from 'react';
 
 export default function CategoryChooserTab() {
   const dispatch = useDispatch()
-  const category = useSelector((state: RootState) => state.categories.value)
-  const pressedElement = useRef()
   const btnArray = [
     <TouchableWithoutFeedback key={0} onPress={() => {
       dispatch(changeCategory("anime_manga"));
       changeBtnOrder(btnArray[1])
 
     }}>
-      <Animated.View style={[styles.button, styles.first]}>
+      <View style={[styles.button, styles.first]}>
         <MaterialIcons name="movie" size={30} color="white" />
-      </Animated.View>
+      </View>
     </TouchableWithoutFeedback>,
 
     <TouchableWithoutFeedback key={1} onPress={(e) => {
       dispatch(changeCategory("games"));
       changeBtnOrder(btnArray[2])
     }}>
-      <Animated.View style={[styles.button, styles.second]}>
+      <View style={[styles.button, styles.second]}>
         <MaterialCommunityIcons name="syllabary-hiragana" size={30} color="white" />
-      </Animated.View>
+      </View>
     </TouchableWithoutFeedback>,
 
     <TouchableWithoutFeedback key={2} onPress={(e) => {
       dispatch(changeCategory("movies_series"));
       changeBtnOrder(btnArray[0])
     }}>
-      <Animated.View style={[styles.button, styles.third]}>
+      <View style={[styles.button, styles.third]}>
         <Ionicons name="game-controller" size={30} color="white" />
-      </Animated.View>
+      </View>
     </TouchableWithoutFeedback>
   ]
   const [btnOrdered, changeBtnOrder] = useState(btnArray[0])
 
-  return React.createElement(View, { style: styles.container }, btnOrdered)
+  return React.createElement(View, {style: styles.container}, btnOrdered)
 }
 
 const styles = StyleSheet.create({
