47e5b4c92488e5d2af48943addf883a314ed710e
JordanMalekeraVives <91864963+jordanmalekera@users.noreply.github.com>
2022-11-05 00:08:29 +0100

    Improved navigation headers and naming

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,26 +1,26 @@
 //import navigation
-import { NavigationContainer, DefaultTheme, DarkTheme, NavigationProp } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme, DarkTheme, NavigationProp, useRoute, useNavigation } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import LinkingConfiguration from './LinkingConfiguration';
 
 //import screens and components
-import Discover from '../screens/Discover';
+import Discover from '../screens/Home';
 import Search from '../screens/Search';
 import Lists from '../screens/Lists';
 import Profile from '../screens/Profile';
 import CategoryChooserTab from '../components/CategoryChooserTab';
 
 //import app theme colors
-import { Alert, BackHandler, ColorSchemeName, DynamicColorIOS, StyleSheet } from 'react-native';
+import { Alert, BackHandler, ColorSchemeName, DynamicColorIOS, StyleSheet, TouchableOpacity } from 'react-native';
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
 
 //import icon
 import { FontAwesomeIcon, } from '@fortawesome/react-native-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faHouse, faSearch, faList, faUser } from '@fortawesome/free-solid-svg-icons';
-import {} from '@fortawesome/free-regular-svg-icons'
+import { faHouse, faSearch, faList, faUser, faGear } from '@fortawesome/free-solid-svg-icons';
+import { } from '@fortawesome/free-regular-svg-icons'
 
 //import types
 import { RootStackParamList, RootTabParamList } from '../types/navigationTypes';
@@ -55,15 +55,8 @@ function RootNavigator() {
     return (
         <Stack.Navigator
             initialRouteName="Root"
-            >
-            <Stack.Screen name="Root" component={BottomTabNavigator} options={{
-                headerTitle: '',
-                headerStyle: { backgroundColor: Colors[colorScheme].background },
-                headerShadowVisible: false,
-                headerBackButtonMenuEnabled: false,
-
-                headerLeft: () => <EmptyComponent />
-            }} />
+        >
+            <Stack.Screen name="Root" component={BottomTabNavigator} options={{ headerShown: false }} />
             <Stack.Group screenOptions={{ headerShown: false, animation: 'slide_from_right', animationDuration: 20 }}>
                 <Stack.Screen name="Intro" component={Intro} />
                 <Stack.Screen name="Login" component={Login} />
@@ -79,54 +72,66 @@ function RootNavigator() {
 
 const BottomTab = createBottomTabNavigator<RootTabParamList>();
 
-export function BottomTabNavigator({navigation}: any) {
+export function BottomTabNavigator({ navigation }: any) {
     const colorScheme = useColorScheme();
     const EmptyComponent = () => null;
-    library.add(faHouse, faSearch, faList, faUser)
-    
+    library.add(faHouse, faSearch, faList, faUser, faGear)
+
     useEffect(
         () =>
-        navigation.addListener('beforeRemove', (e:any) => {
-            e.preventDefault();
+            navigation.addListener('beforeRemove', (e: any) => {
+                e.preventDefault();
 
-            Alert.alert('Exit',
-                'Do you want to exit the app?',
-                [
-                  { text: "Don't exit", style: 'cancel', onPress: () => {} },
-                  {
-                    text: 'Exit',
-                    style: 'destructive',
-                    // If the user confirmed, then we dispatch the action we blocked earlier
-                    // This will continue the action that had triggered the removal of the screen
-                    onPress: () => BackHandler.exitApp()
-                  },
-                ]
-              );
-    }))
+                Alert.alert('Exit',
+                    'Do you want to exit the app?',
+                    [
+                        { text: "Don't exit", style: 'cancel', onPress: () => { } },
+                        {
+                            text: 'Exit',
+                            style: 'destructive',
+                            // If the user confirmed, then we dispatch the action we blocked earlier
+                            // This will continue the action that had triggered the removal of the screen
+                            onPress: () => BackHandler.exitApp()
+                        },
+                    ]
+                );
+            }))
+
+    function SettingsBtn({ btnColor }: any) {
+        return (
+            <TouchableOpacity>
+                <FontAwesomeIcon icon="gear" color={btnColor} size={20} />
+            </TouchableOpacity>
+        )
+    }
 
     return (
         <BottomTab.Navigator
-            initialRouteName="Discover"
+            initialRouteName="Home"
             backBehavior="history"
             screenOptions={{
-                headerTitleStyle: { fontFamily: 'open-sans', fontSize: 34, fontWeight: 'bold' },
+                headerTitleStyle: { fontFamily: 'open-sans', fontSize: 22, fontWeight: 'bold' },
                 headerStyle: { backgroundColor: Colors[colorScheme].background },
+                headerRightContainerStyle: { paddingRight: '10%' },
+                headerLeftContainerStyle: { paddingLeft: '8%' },
+                headerTitleAlign: 'center',
+                headerRight: () => <SettingsBtn btnColor={Colors[colorScheme].tint} />,
                 headerShadowVisible: false,
-                tabBarStyle: { backgroundColor: Colors[colorScheme].background, borderTopWidth: 0, shadowColor: '' },
+                tabBarStyle: { backgroundColor: Colors[colorScheme].background, borderTopWidth: 0, shadowColor: 'transparent' },
                 tabBarLabelStyle: { display: 'none' },
                 tabBarActiveTintColor: Colors[colorScheme].tint
             }}>
             <BottomTab.Screen
-                name="Discover"
+                name="Home"
                 component={Discover}
                 options={{
-                    tabBarIcon: ({ color }) => <FontAwesomeIcon icon='house' color={color} size={24}/>,
+                    tabBarIcon: ({ color }) => <FontAwesomeIcon icon='house' color={color} size={22} />,
                 }} />
             <BottomTab.Screen
                 name="Search"
                 component={Search}
                 options={{
-                    tabBarIcon: ({ color }) => <FontAwesomeIcon icon="search" color={color} size={24} />
+                    tabBarIcon: ({ color }) => <FontAwesomeIcon icon="search" color={color} size={22} />
                 }} />
             <BottomTab.Screen
                 name="Chooser"
@@ -140,13 +145,13 @@ export function BottomTabNavigator({navigation}: any) {
                 name="Lists"
                 component={Lists}
                 options={{
-                    tabBarIcon: ({ color }) => <FontAwesomeIcon icon="list" color={color} size={24} />
+                    tabBarIcon: ({ color }) => <FontAwesomeIcon icon="list" color={color} size={22} />
                 }} />
             <BottomTab.Screen
                 name="Profile"
                 component={Profile}
                 options={{
-                    tabBarIcon: ({ color }) => <FontAwesomeIcon icon="user" color={color} size={24} />
+                    tabBarIcon: ({ color }) => <FontAwesomeIcon icon="user" color={color} size={22} />
                 }} />
         </BottomTab.Navigator>
     );
