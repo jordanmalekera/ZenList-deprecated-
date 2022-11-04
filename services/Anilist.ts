62331efdbbec7f18f94ea3e89fb06a91bab8d3f9
JordanMalekeraVives <91864963+jordanmalekera@users.noreply.github.com>
2022-11-05 00:09:22 +0100

    Improved anime service

--- services/Anilist.ts
+++ services/Anilist.ts
@@ -1,7 +1,7 @@
 import API from "./Api";
 import axios, { AxiosResponse } from "axios";
 import { Alert } from "react-native";
-import { AniResponse, MediaType, Page } from "../types/AnilistTypes";
+import { AniResponse, MediaSort, MediaType, Page } from "../types/AnilistTypes";
 
 const http = axios.create();
 http.defaults.baseURL = 'https://graphql.anilist.co'
@@ -10,7 +10,7 @@ http.defaults.method = 'POST';
 http.defaults.headers['Content-Type'] = 'application/json';
 
 export const anilistService = {
-    getMedias: async (type: MediaType, sort: string) => {
+    getTopMedias: async (type: MediaType, sort: MediaSort) => {
         return http({
             data: {
                 query: `{
@@ -19,8 +19,53 @@ export const anilistService = {
                                 id
                                 title {
                                     english
+                                    romaji
                                 }
-                                type
+                                coverImage {
+                                    large
+                                }
+                            }
+                        }
+                    }`
+            }
+        })
+            .then((response: AxiosResponse) => { console.log(response.data); return response.data })
+            .catch((error) => Alert.alert("Error", error.toString()))
+    },
+    getCurrentSeasonMedias: async (type: MediaType, sort: MediaSort) => {
+        const current = new Date();
+        const currentYear = current.getFullYear()
+        const currentMonth = current.getMonth();
+        let season
+        switch (true) {
+            case (currentMonth === 12 || currentMonth <= 2):
+                season = 'WINTER'
+                break;
+            case (currentMonth >= 3 && currentMonth <= 5):
+                season = 'SPRING'
+                break;
+            case (currentMonth >= 6 && currentMonth <= 8):
+                season = 'SUMMER'
+                break;
+            case (currentMonth >= 9 && currentMonth <= 11):
+                season = 'FALL'
+                break;
+            default:
+                season = ''
+        }
+        return http({
+            data: {
+                query: `{
+                        Page(page: 1) {
+                            media(type: ${type}, season: ${season}, seasonYear: ${currentYear} sort: ${sort}) {
+                                id
+                                title {
+                                    english
+                                    romaji
+                                }
+                                coverImage {
+                                    large
+                                }
                             }
                         }
                     }`
