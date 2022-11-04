676802b29292fba4d0c874b578fd5b7e22982606
JordanMalekeraVives <91864963+jordanmalekera@users.noreply.github.com>
2022-11-04 17:26:17 +0100

    Added query provider

--- App.tsx
+++ App.tsx
@@ -6,22 +6,26 @@ import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 import { Provider } from 'react-redux'
 import { store } from './app/store';
+import { QueryClientProvider, QueryClient } from 'react-query';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  const queryClient = new QueryClient();
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
       <SafeAreaProvider>
-          <Provider store={store}>
-          <Navigation colorScheme={colorScheme} />
-          <StatusBar />
-      </Provider>
-        </SafeAreaProvider>
-        
+        <Provider store={store}>
+          <QueryClientProvider client={ queryClient }>
+            <Navigation colorScheme={colorScheme} />
+            <StatusBar />
+          </QueryClientProvider>
+        </Provider>
+      </SafeAreaProvider>
+
     );
   }
 }
