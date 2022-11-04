23b6e0b617dd96669037e99ff2736edaa19ef41e
JordanMalekeraVives <91864963+jordanmalekera@users.noreply.github.com>
2022-11-04 17:27:09 +0100

    Added first anime list

--- screens/Discover.tsx
+++ screens/Discover.tsx
@@ -1,30 +1,75 @@
-import { Text, View } from '../components/Themed';
-import { StyleSheet } from 'react-native'
-import { useSelector } from 'react-redux';
-import { RootState } from '../app/store';
+import { Text, View, Header } from '../components/Themed';
+import { StyleSheet, TouchableOpacity } from 'react-native'
+import { anilistService } from '../services/Anilist';
+import React from 'react';
+import { AniResponse } from '../types/AnilistTypes';
+import { FlashList } from '@shopify/flash-list';
+import { useQuery } from 'react-query';
+
+
 
 export default function Discover() {
-    const category = useSelector((state: RootState) => state.categories.value)
-    const chosenCategory = () => {
-        if (category.movies_series) return "Movies";
-        else if (category.anime_manga) return "Anime";
-        else if (category.games) return "Games";
-        else return "";
+
+    const fetchData = async () => {
+        const response: AniResponse = await anilistService.getMedias('ANIME', 'SCORE_DESC')
+        return response.data.Page
     }
-    
-    return(
+
+    const { data, status } = useQuery("top-100", fetchData)
+
+    function List() {
+        if (status === "loading") {
+            return <Text>Loading...</Text>
+        }
+        else if (status === 'error' || data == undefined) {
+            return <Text>Error</Text>
+        }
+        else {
+            return (
+                <FlashList
+                    data={data.media}
+                    renderItem={({ item }) => <Text style={{ color: 'white' }}>{item.title.english}</Text>}
+                    keyExtractor={item => item.id.toString()}
+                    horizontal={true}
+                    estimatedItemSize={226}
+                    ListEmptyComponent={<Text>No results</Text>}
+                />)
+        }
+    }
+    return (
         <View style={styles.view}>
-            <Text>{chosenCategory()}</Text>
+            <TouchableOpacity>
+                <Header>Top 100</Header>
+            </TouchableOpacity>
+            <List />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     view: {
-        height: "100%",
+        height: "20%",
+        width: '100%'
     },
     img: {
         width: 20,
         height: 20
+    },
+    container: {
+        flex: 1
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 32,
+    },
+    list: {
+        backgroundColor: 'red',
+        width: '100%',
+        height: "100%"
     }
 })
