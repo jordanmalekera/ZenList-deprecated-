736faedf6508b108902ab46e7436868518c29a7b
JordanMalekeraVives <91864963+jordanmalekera@users.noreply.github.com>
2022-11-04 14:02:02 +0100

    Added anilist types

---
+++ types/AnilistTypes.ts
@@ -0,0 +1,117 @@
+export interface Page {
+    pageInfo: PageInfo,
+    users: undefined,
+    media: Media[]
+    characters: undefined,
+}
+
+export interface PageInfo {
+    total: number,
+    perPage: number,
+    currentPage: number,
+    lastPage: number,
+    hasNextPage: boolean
+}
+
+// All types for Media
+export interface Media {
+    id: number,
+    idMal: number,
+    title: MediaTitle,
+    type: MediaType,
+    format: MediaFormat,
+    status: MediaStatus,
+    description: string,
+    startDate: Date,
+    endDate: Date,
+    season: MediaSeason,
+    seasonYear: number,
+    seasonInt: number,
+    episodes: number,
+    duration: number,
+    chapters: number,
+    volumes: number,
+    countryOfOrigin: string,
+    isLicensed: boolean,
+    source: MediaSource,
+    hashtag: string,
+    trailer: MediaTrailer,
+    updatedAt: number,
+    coverImage: MediaCoverImage,
+    bannerImage: string,
+    genres: string[],
+    synonyms: string[],
+    averageScore: number,
+    meanScore: number,
+    popularity: number,
+    isLocked: boolean,
+    trending: number,
+    favourites: number,
+    tags: MediaTag[],
+    // relations: MediaConnection,
+    // characters: CharacterConnection,
+    // staff: StaffConnection,
+    // studios: StudioConnection,
+    isFavourite: boolean,
+    isFavouriteBlocked: boolean,
+    isAdult: boolean,
+    // nextAiringEpisode: AiringSchedule,
+    // airingSchedule: AiringScheduleConnection,
+    // trends: MediaTrendConnection,
+    // externalLinks: MediaExternalLink[],
+    // streamingEpisodes: MediaStreamingEpisode[],
+    // rankings: MediaRank[],
+    // mediaListEntry: MediaList,
+    // reviews: ReviewConnection,
+    // recommendations: RecommendationConnection,
+    // stats: MediaStats,
+    siteUrl: string,
+    autoCreateForumThread: boolean,
+    isRecommendationBlocked: boolean,
+    isReviewBlocked: boolean,
+    modNotes: string,
+}
+
+export interface MediaTitle {
+    romaji?: string,
+    english?: string,
+    native?: string,
+    userPreferred?: string
+}
+
+export interface MediaTrailer {
+    id: string,
+    site: string,
+    thumbnail: string
+}
+
+export interface MediaCoverImage {
+    extraLarge: string,
+    large: string,
+    medium: string,
+    color: string
+}
+
+export interface MediaTag {
+    id: number,
+    name: string,
+    description: string,
+    category: string,
+    rank: number,
+    isGeneralSpoiler: boolean,
+    isMediaSpoiler: boolean,
+    isAdult: boolean,
+    userId: number
+}
+
+export type MediaType = 'ANIME' | 'MANGA';
+
+export type MediaFormat = 'TV' | 'TV_SHORT' | 'MOVIE' | 'SPECIAL' | 'OVA' | 'ONA' | 'MUSIC' | 'MANGA' | 'NOVEL' | 'ONE_SHOT';
+
+export type MediaStatus = 'FINISHED' | 'RELEASING' | 'NOT_YET_RELEASED' | 'CANCELLED' | 'HIATUS';
+
+export type MediaSeason = 'WINTER' | 'SPRING' | 'SUMMER' | 'FALL';
+
+export type MediaSource = 'ORIGINAL' | 'MANGA' | 'LIGHT_NOVEL' | 'VISUAL_NOVEL' | 'VIDEO_GAME' | 'OTHER' | 'NOVEL' | 'DOUJINSHI' | 'ANIME' | 'WEB_NOVEL' | 'LIVE_ACTION' | 'GAME' | 'COMIC' | 'MULTIMEDIA_PROJECT' | 'PICTURE_BOOK';
+
+// All types for Recomm
