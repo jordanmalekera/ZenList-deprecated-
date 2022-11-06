783cbaffe7de9c316f581531d7a9d9b0a2f96cf1
JordanMalekeraVives <91864963+jordanmalekera@users.noreply.github.com>
2022-11-06 14:09:47 +0100

    Improved code and added slider (not auto)

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,61 +1,34 @@
 import { Text, View, Header, ScrollView } from '../components/Themed';
-import { StyleSheet, TouchableOpacity, Image } from 'react-native'
+import { StyleSheet, TouchableOpacity, Image, RefreshControl, Dimensions } from 'react-native'
 import { anilistService } from '../services/Anilist';
-import React from 'react';
+import React, { useState } from 'react';
 import { AniResponse, Media } from '../types/AnilistTypes';
 import { FlashList } from '@shopify/flash-list';
 import { useQuery } from 'react-query';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
-import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
 
 export default function Discover() {
     const colorScheme = useColorScheme();
 
 
-    function List({ content }: any) {
+    function Lister({ content, func, style, flashListProps }: any) {
         const fetchData = async () => {
-            if (content === 'topList') {
-                const response: AniResponse = await anilistService.getTopMedias('ANIME', 'SCORE_DESC')
-                return response.data.Page
-            }
-            else if (content === 'seasonList') {
-                const response: AniResponse = await anilistService.getCurrentSeasonMedias('ANIME', 'POPULARITY_DESC')
-                return response.data.Page
-            }
-            else if (content === 'trendingList') {
-                const response: AniResponse = await anilistService.getTopMedias('ANIME', 'TRENDING_DESC')
-                return response.data.Page
-            }
-            else {
-                return
-            }
+            const response: AniResponse = await func;
+            return response.data.Page
         }
 
         const { data, status } = useQuery(content, fetchData)
-        if (status === "loading") {
-            return <Text>Loading...</Text>
-        }
-        else if (status === "idle") {
-            return <Text>Idle...</Text>
-        }
-        else if (status === 'error' || data == undefined) {
-            return <Text>Error</Text>
-        }
+        if (status === "loading") return <Text>Loading...</Text>
+        else if (status === "idle") return <Text>Idle...</Text>
+        else if (status === 'error' || data == undefined) return <Text>Error</Text>
         else {
             function ListItem({ item }: any) {
-                let text;
-                if(item.title.english) {
-                    text = <Text numberOfLines={2} style={styles.mediaTitle}>{item.title.english}</Text>
-                }
-                else {
-                    text = <Text numberOfLines={2} style={styles.mediaTitle}>{item.title.romaji}</Text>
-                }
+                let title;
+                (item.title.english) ? title = item.title.english : title = item.title.romaji
                 return (
-                    <TouchableOpacity style={styles.listItem}>
-                        <Image source={{ uri: item.coverImage.large }} style={styles.coverImage}></Image>
-                        {text}
+                    <TouchableOpacity style={style.listItem}>
+                        <Image source={{ uri: item.coverImage.extraLarge }} style={style.coverImage}></Image>
+                        <Text numberOfLines={2} style={style.mediaTitle}>{title}</Text>
                     </TouchableOpacity>
                 )
             }
@@ -65,50 +38,84 @@ export default function Discover() {
                     <FlashList
                         data={data.media}
                         renderItem={({ item }) => <ListItem item={item} />}
-                        keyExtractor={item => item.id.toString()}
+                        keyExtractor={(item: Media) => item.id.toString()}
                         horizontal={true}
                         estimatedItemSize={129}
                         ListEmptyComponent={<Text>No results</Text>}
+                        showsHorizontalScrollIndicator={false}
+                        {...flashListProps}
                     />
                 </View>
             )
         }
     }
 
-    return (
-        <ScrollView style={styles.view} >
-            <TouchableOpacity style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingTop: '4%', paddingBottom: '8%' }}>
-                <Header>Trending now</Header>
-                <FontAwesomeIcon icon={faArrowRight} color={Colors[colorScheme].tint} />
-            </TouchableOpacity>
-            <List content='trendingList' />
-            
-            <TouchableOpacity style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingTop: '14%', paddingBottom: '8%' }}>
-                <Header>Popular this season</Header>
-                <FontAwesomeIcon icon={faArrowRight} color={Colors[colorScheme].tint} />
-            </TouchableOpacity>
-            <List content='seasonList' />
+    interface ListProps { title: string, data: Promise<any>, style: any }
+    function List({ title, data, style }: ListProps) {
+        return (
+            <>
+                <TouchableOpacity style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingTop: '14%', paddingBottom: '8%', paddingHorizontal: '4%' }}>
+                    <Header>{title}</Header>
+                    <Text>See all</Text>
+                </TouchableOpacity>
+                <Lister content={title} func={data} style={style} />
+            </>
+        )
+    }
 
-            <TouchableOpacity style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingTop: '14%', paddingBottom: '8%' }}>
-                <Header>Top 100</Header>
-                <FontAwesomeIcon icon={faArrowRight} color={Colors[colorScheme].tint} />
-            </TouchableOpacity>
-            <List content='topList' />
+    function Slide({ data, style }: ListProps) {
+        return <Lister content={'Slide'} func={data} style={style} flashListProps={{ snapToAlignment: "start", decelerationRate: "normal", snapToInterval: Dimensions.get("window").width }} />
+    }
+
+    const defaultStyle = { listItem: styles.listItem, coverImage: styles.coverImage, mediaTitle: styles.mediaTitle }
+    const slideStyle = { listItem: styles.slideListItem, coverImage: styles.slideCoverImage, mediaTitle: styles.slideMediaTitle }
+    return (
+        <ScrollView style={styles.view}>
+            <Slide
+                title=''
+                data={anilistService.getTopMedias('ANIME', 'TRENDING_DESC')}
+                style={slideStyle} />
+            <List
+                title='New anime'
+                data={anilistService.getTopMedias('ANIME', 'START_DATE_DESC')}
+                style={defaultStyle} />
+            <List
+                title='Popular this season'
+                data={anilistService.getCurrentSeasonMedias('ANIME', 'POPULARITY_DESC')}
+                style={defaultStyle} />
+            <List
+                title='Top 100'
+                data={anilistService.getTopMedias('ANIME', 'SCORE_DESC')}
+                style={defaultStyle} />
         </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
     view: {
-        height: "34%",
         width: '100%',
-        paddingHorizontal: '2%'
+        paddingTop: 20
+
+    },
+    slideListItem: {
+        width: Dimensions.get("window").width,
+        flexDirection: 'column',
+        paddingHorizontal: 12
+    },
+    slideCoverImage: {
+        height: Dimensions.get("window").height / 3.8,
+        borderRadius: 12,
+        marginBottom: 10
+    },
+    slideMediaTitle: {
+        flexWrap: 'wrap'
     },
     listItem: {
         width: 130,
-        flexDirection: 'column',
-        paddingEnd: 24,
-        // paddingHorizontal: 12,
+        paddingHorizontal: 12
+    },
+    listTitle: {
+        paddingHorizontal: '4%'
     },
     coverImage: {
         height: 140,
