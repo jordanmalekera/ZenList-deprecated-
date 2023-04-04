8643ffa8cb40e15e1d6d72c33898139398491788
JordanMalekeraVives <91864963+jordanmalekera@users.noreply.github.com>
2023-04-04 15:37:51 +0200

    Refactored to enums

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -2,13 +2,14 @@ import { Text, View } from '../components/Themed';
 import { StyleSheet } from 'react-native'
 import { useSelector } from 'react-redux';
 import { RootState } from '../app/store';
+import { Category } from '../features/categorySlice';
 
 export default function Profile() {
     const category = useSelector((state: RootState) => state.categories.value)
     const chosenCategory = () => {
-        if (category.movies_series) return "Movies";
-        else if (category.anime_manga) return "Anime";
-        else if (category.games) return "Games";
+        if (category == Category.ANIME_MANGA) return "Movies";
+        else if (category == Category.MOVIES_SERIES) return "Anime";
+        else if (category == Category.GAMES) return "Games";
         else return "";
     }
     
