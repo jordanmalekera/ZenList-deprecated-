433ad17f7782316adee7280254c991b5688a2da8
JordanMalekeraVives <91864963+jordanmalekera@users.noreply.github.com>
2023-04-07 14:45:42 +0200

    Made call method dynamic

--- services/TMDB.ts
+++ services/TMDB.ts
@@ -1,22 +1,24 @@
 import axios, { AxiosResponse } from "axios";
 import { Alert } from "react-native";
+import { TMDBPage } from "../types/TMDBTypes";
 import { TMDB_AUTH } from "./API";
 
 const http = axios.create();
-http.defaults.baseURL = 'https://api.themoviedb.org/3/'
+http.defaults.baseURL = 'https://api.themoviedb.org/3'
 http.defaults.responseType = 'json';
-http.defaults.headers.common = {'api_key': TMDB_AUTH.KEY}
+http.defaults.params = {'api_key': TMDB_AUTH.KEY};
 
+type TMDBEndpoint = "/account" | "/authentication" | "/certification" | "/collection" | "/company" | "/configuration" | "/credit" | "/discover" | "/find" | "/genre" | "/guest_session" | "/keyword" | "/list" | "/movie" | "/network" | "/trending" | "/person" | "/review" | "/search" | "/tv" | "/watch";
 export const TMDBService = {
-    getMovie: async (movieId: number) => {
-        return http.get('movie/' + movieId)
-        .then((response: AxiosResponse) => { console.log(response.data); return response.data })
-        .catch((error) => Alert.alert("Error", error.toString()))
-    },
-    getPopularMovies: async () => {
-        return http.get('movie/popular')
-        .then((response: AxiosResponse) => { console.log(response.data); return response.data })
-        .catch((error) => Alert.alert("Error", error.toString()))
+    get: async (endpoint: TMDBEndpoint, id?: number | string, propEndpoint?: string) => {
+        let request = endpoint;
+        if (id) request += "/" + id;
+        if (propEndpoint) request += propEndpoint;
+        return http.get(request)
+            .then((response: AxiosResponse<TMDBPage>) => { return response.data.results })
+            .catch((error) => {
+                Alert.alert("Error", error.toString())
+                console.log(error)
+        })      
     }
-
 }
