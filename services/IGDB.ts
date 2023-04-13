25a91e8b6c335095aef95f48b80eedce8f439c74
JordanMalekeraVives <91864963+jordanmalekera@users.noreply.github.com>
2023-04-13 13:27:50 +0200

    Added id to call

--- services/IGDB.ts
+++ services/IGDB.ts
@@ -10,8 +10,10 @@ http.defaults.headers.common["Client-Id"] = IGDB_AUTH.CLIENT_ID;
 http.defaults.headers.common["Authorization"] = IGDB_AUTH.ACCES_TOKEN;
 
 export const IGDBService = {
-    get: async (endpoint: string) => {
-        const data = "fields *, cover.url; sort rating desc; limit 20;"
+    get: async (endpoint: string, id?: number) => {
+        let data;
+        if (id) data = `fields *, cover.url, genres.name; where id = ${id};`;
+        else data = `fields *, cover.url;`;
         return http.post(endpoint, data)
             .then((response: any) => {
                 return response.data
