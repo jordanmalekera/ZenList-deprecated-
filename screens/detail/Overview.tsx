5eaf6b7f540885a2dd9f8e62b57d1deeaa887b12
JordanMalekeraVives <91864963+jordanmalekera@users.noreply.github.com>
2023-11-03 13:39:25 +0100

    Added TMDBMovie

--- screens/detail/Overview.tsx
+++ screens/detail/Overview.tsx
@@ -2,8 +2,25 @@ import { StyleSheet, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { View, Text, Header } from '../../components/Themed'
 import { AniMedia, AniMediaTrailer } from '../../types/ANIL/interfaces'
+import { Category } from '../../features/categorySlice'
+import { TMDBMovie } from '../../types/TMDB/interfaces'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../app/store'
 
-const Overview = ({ data }: { data: AniMedia }) => {
+const Overview = ({ data }: { data: any }): any => {
+    const category = useSelector((state: RootState) => state.categories.value)
+
+    if (category == Category.ANIME_MANGA) {
+        return aniOverview(data)
+    } else if (category == Category.MOVIES_SERIES) {
+        return tmdbOverview(data)
+    } else {
+        return <View ></View>
+    }
+
+}
+
+const aniOverview = (data: AniMedia) => {
     const sypnosis = data.description.split(/<[^>]+>/g);
     return (
         <View style={styles.view}>
@@ -22,6 +39,55 @@ const Overview = ({ data }: { data: AniMedia }) => {
 
             </View>
             {(data.trailer) ? <Trailers trailer={data.trailer} /> : null}
+        </View>
+    )
+}
+
+const tmdbOverview = (data: TMDBMovie) => {
+    console.warn(data);
+    const date = new Date(data.release_date);
+    const months = ["Januari","Februari","March","April","May","June","July","August","September","October","November","December",]
+    const modDate = `${months[date.getMonth() + 1].substring(0, 3)} ${date.getDate()}, ${date.getFullYear()}`
+    return (
+        <View style={styles.view}>
+            <TouchableOpacity style={styles.header}>
+                <Header>Sypnosis</Header>
+                <Text>See more</Text>
+            </TouchableOpacity>
+            <Text numberOfLines={3} style={(data.production_companies) ? { marginBottom: 18 } : null}>{data.overview}</Text>
+            {/* {(data.studios.nodes[0]) ? <Text>{"Studio: " + data.studios.nodes[0].name}</Text> : null} */}
+            {(data.production_companies) ? <Text>{"Producers: " + data.production_companies.map((company) => company.name).join(", ")}</Text> : null}
+            <TouchableOpacity style={styles.header}>
+                <Header>Details</Header>
+                <Text>See more</Text>
+            </TouchableOpacity>
+            <View style={styles.details}>
+                <View style={{ display: 'flex', flexDirection: "row", justifyContent: "space-between", width: "46%"}}>
+                    <View style={{ display: 'flex', flexDirection: "column" }}>
+                        <Text style={{ fontWeight: "bold" }}>Episodes:</Text>
+                        <Text style={{ fontWeight: "bold" }}>Type:</Text>
+                        <Text style={{ fontWeight: "bold" }}>Start:</Text>
+                    </View>
+                    <View style={{ display: 'flex', flexDirection: "column", alignItems: "flex-end"}}>
+                        <Text >1</Text>
+                        <Text></Text>
+                        <Text>{modDate}</Text>
+                    </View>
+                </View>
+                <View style={{ display: 'flex', flexDirection: "row", justifyContent: "space-between", width: "46%"}}>
+                    <View style={{ display: 'flex', flexDirection: "column" }}>
+                        <Text style={{ fontWeight: "bold" }}>Status:</Text>
+                        <Text style={{ fontWeight: "bold" }}>Score:</Text>
+                        <Text style={{ fontWeight: "bold" }}>End:</Text>
+                    </View>
+                    <View style={{ display: 'flex', flexDirection: "column", alignItems: "flex-end" }}>
+                        <Text></Text>
+                        <Text></Text>
+                        <Text>{modDate}</Text>
+                    </View>
+                </View>
+            </View>
+            {/* {(data.video) ? <Trailers trailer={data.} /> : null} */}
         </View>
     )
 }
@@ -57,7 +123,9 @@ const styles = StyleSheet.create({
         paddingBottom: 8
     },
     details: {
-
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: 'space-between'
     },
     trailerContainer: {
         display: 'flex',
